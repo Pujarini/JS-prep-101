f64ae42dc400a4fae5b897946ef0c50af0f06acd
Pujarini Jena <pujarinijena@Pujarini-MacBook-Pro.local>
2022-09-07 23:01:18 +0530

    added complete feature to to-do

--- machine-coding-problems/to-do-app/index.js
+++ machine-coding-problems/to-do-app/index.js
@@ -3,7 +3,24 @@ const addBtn = document.querySelector(".add-btn");
 const newTaskDiv = document.querySelector(".new-task");
 
 const tasks = [];
+let taskItems = [];
 newTaskDiv.innerHTML = "You got no tasks";
+
+const completeTodo = (items) => {
+  items.forEach((item) => {
+    item.addEventListener("click", () => {
+      item.classList.add("complete");
+    });
+  });
+};
+
+// const deleteToDo = (taskItems) => {
+// items.forEach((item) => {
+//   item.addEventListener("click", () => {
+//     item.classList.add("complete");
+//   });
+// });
+// };
 
 const addToDo = () => {
   let todo = input.value;
@@ -11,19 +28,26 @@ const addToDo = () => {
 };
 const showTodos = (taskList) => {
   tasks.forEach((task) => {
-    taskItems.push(task);
     const newtask = document.createElement("li");
     newtask.innerText = task;
     newtask.classList.add("item");
+
+    const btn = document.createElement("button");
+    btn.innerHTML = ` <i class="fa-solid fa-xmark"></i>`;
+    newtask.appendChild(btn);
     taskList.appendChild(newtask);
   });
+  const taskItems = newTaskDiv.querySelectorAll(".item");
+  if (taskItems.length > 0) {
+    completeTodo(taskItems);
+    // deleteToDo(taskItems);
+  }
 };
 
 addBtn.addEventListener("click", (e) => {
   e.preventDefault();
   if (input.value) {
     newTaskDiv.innerHTML = "";
-    console.log(newTaskDiv);
     const taskList = document.createElement("ul");
     newTaskDiv.appendChild(taskList);
     addToDo();
@@ -31,11 +55,3 @@ addBtn.addEventListener("click", (e) => {
   }
   input.value = "";
 });
-
-// console.log(taskItems);
-
-// taskItems.forEach((item) => {
-//   item.addEventListener("click", () => {
-//     item.classList.add("complete");
-//   });
-// });
