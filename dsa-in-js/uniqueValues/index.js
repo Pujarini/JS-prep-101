ee1bc8c470e5e3cf27b0d6012391aa9f132b5ef6
Pujarini Jena <pujarinijena@Pujarini-MacBook-Pro.local>
2022-10-20 22:08:07 +0530

    sliding window and unique value

---
+++ dsa-in-js/uniqueValues/index.js
@@ -0,0 +1,14 @@
+function countUniqueValue(arr) {
+  if (arr.length === 0) return 0;
+  let i = 0;
+  for (let j = 0; j < arr.length; j++) {
+    if (arr[i] === arr[j]) {
+      i++;
+      arr[i] = arr[j];
+    }
+  }
+  return i + 1;
+}
+
+console.log(countUniqueValue([-1, 0, 1, 2, 3])); // O(N)
+// countUniqueValue([-1, 0, 1, 2, 3]);
