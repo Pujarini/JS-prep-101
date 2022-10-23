b86aa1d02ab9ca1d7b2ad28dcdd8abce5ce5bfdb
Pujarini Jena <pujarinijena@Pujarini-MacBook-Pro.local>
2022-10-23 18:33:42 +0530

    unique value algo

--- dsa-in-js/uniqueValues/index.js
+++ dsa-in-js/uniqueValues/index.js
@@ -1,7 +1,7 @@
 function countUniqueValue(arr) {
   if (arr.length === 0) return 0;
   let i = 0;
-  for (let j = 0; j < arr.length; j++) {
+  for (let j = 1; j < arr.length; j++) {
     if (arr[i] === arr[j]) {
       i++;
       arr[i] = arr[j];
